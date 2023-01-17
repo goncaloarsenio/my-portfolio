bdb6addb6943fbe50d136ad1783851512117c509
goncaloarsenio <goncalo.arsenio92@gmail.com>
2023-01-17 22:47:06 +0000

    links: complete

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -12,7 +12,7 @@ const SocialLinks = () => {
           LinkedIn <FaLinkedin size={30} />
         </>
       ),
-      href: "https://linkedin.com",
+      href: "https://www.linkedin.com/in/goncalo-arsenio-/",
       style: "rounded-tr-md",
     },
     {
@@ -40,25 +40,35 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/public/resume.pdf",
+      href: "/resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
   ];
 
   return (
-    <div className="flex flex-col top-[35%] left-0 fixed">
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
       <ul>
-        <li className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500">
-          <a
-            href=""
-            className="flex justify-between items-center w-full text-white "
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            key={id}
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
+              " " +
+              style
+            }
           >
-            <>
-              LinkedIn <FaLinkedin size={30} />
-            </>
-          </a>
-        </li>
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white "
+              download={download}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <>{child}</>
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
